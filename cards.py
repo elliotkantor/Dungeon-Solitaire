ab502a4c4191f4a2124e7cf4eca15497e18e8f2d
Elliot Kantor <elliotjkantor@gmail.com>
2021-02-04 10:05:04 -0500

    Tweaked card class and started dungeonSolitaire.py

--- cards.py
+++ cards.py
@@ -10,6 +10,7 @@ suits = "Diamonds Spades Hearts Clubs".split()
 # class for a singular card
 class Card:
     """Card object to add card functionality"""
+
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
@@ -18,14 +19,6 @@ class Card:
         else:
             self.name = self.value + " " + self.suit
 
-# make a list of all cards, in a sorted order
-allCards = [Card(value, suit)
-    for suit in suits 
-    for value in values]
-
-if hasJokers:
-    allCards.append("Black Joker")
-    allCards.append("Red Joker")
 
 def reshuffle():
     """Shuffle all cards and remove cards from the discard"""
@@ -34,7 +27,6 @@ def reshuffle():
     random.shuffle(availableCards)
     discard = []
 
-reshuffle()
 
 def drawCard():
     """Return one random card from the deck and move the card to the discard pile"""
@@ -43,8 +35,11 @@ def drawCard():
     discard.append(chosenCard)
     return chosenCard
 
+
 def dealCards(cardsPerPlayer, numPlayers, numDecks=1):
-    assert cardsPerPlayer * numPlayers <= len(availableCards) * numDecks, f"There are only {len(availableCards) * numDecks} cards but you need {cardsPerPlayer * numPlayers} cards."
+    assert (
+        cardsPerPlayer * numPlayers <= len(availableCards) * numDecks
+    ), f"There are only {len(availableCards) * numDecks} cards but you need {cardsPerPlayer * numPlayers} cards."
     playerHands = []
     for player in range(numPlayers):
         singleHand = []
@@ -52,12 +47,24 @@ def dealCards(cardsPerPlayer, numPlayers, numDecks=1):
             singleHand.append(drawCard())
         playerHands.append(singleHand)
     return playerHands
-    
+
+
 def discard(card):
     discard.append(card)
     availableCards.remove(card)
+
 
 # if __name__ == "__main__":
 #     hands = dealCards(5, 3)
 #     for hand in hands:
 #         print(hand)
+
+# make a list of all cards, in a sorted order
+if __name__ == "__main__":
+    allCards = [Card(value, suit) for suit in suits for value in values]
+
+    if hasJokers:
+        allCards.append(Card("Black", "Joker"))
+        allCards.append(Card("Red", "Joker"))
+
+    reshuffle()
--- dungeonSolitaire.py
+++ dungeonSolitaire.py
@@ -1,2 +1,9 @@
 # dungeon solitaire - recreation of the solitaire game 
 # instructions at: https://matthewlowes.files.wordpress.com/2015/06/dungeon-solitaire.pdf
+import cards
+
+deck = [cards.Card(value, suit) for value in cards.values for suit in cards.suits]
+deck.append(cards.Card("Black", "Joker"))
+
+for card in deck:
+    print(card.name)
