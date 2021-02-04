b9710a0446531b1a9dc5d899e1b31f1c5191bc75
Elliot Kantor <elliotjkantor@gmail.com>
2021-02-04 10:51:02 -0500

    Added structure for checking drawn card type and triggering action

--- dungeonSolitaire.py
+++ dungeonSolitaire.py
@@ -8,6 +8,8 @@ deck.append(cards.Card("Black", "Joker"))
 random.shuffle(deck)
 healthCards = [card for card in deck if card.value in [str(num) for num in range(2, 11)] and card.suit == "Hearts"]
 healthCards.sort(key=lambda x: int(x.value))
+for card in healthCards:
+    deck.remove(card)
 
 def getPlayerHealth():
     playerHealth = int(healthCards[-1].value) - 1
@@ -23,5 +25,38 @@ if __name__ == "__main__":
     # for card in healthCards:
     #     print(card.name)
     while True:
+        # show player stats
+
+        # draw top card from deck
+        drawnCard = deck[-1]
+        deck.remove(drawnCard)
+        if drawnCard.value == "Jack":
+            # add to powerups
+            pass
+        elif drawnCard.value == "Ace":
+            # add burnt torch
+            pass
+        elif drawnCard.suit == "Joker":
+            # obtain scroll of light
+            pass
+        elif drawnCard.value == "Queen":
+            # add divine favor if won
+            pass
+        elif drawnCard.value == "King":
+            # add king if won
+            pass
+        elif drawnCard.suit == "Spades":
+            # monster
+            pass
+        elif drawnCard.suit == "Diamonds":
+            # trap / treasure
+            pass
+        else:
+            # clubs - sealed doors
+            pass
+
+        # do action depending on if deck is trap, monster, or door, and repeat until passed
+
+        # save player stats
         playerHealth = getPlayerHealth()
         checkIfLost(burntTorches, len(deck), playerHealth)
