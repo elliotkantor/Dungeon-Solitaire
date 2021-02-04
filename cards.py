08b2e32ead9e173b54730fb8033efa7d95473c01
Elliot Kantor <elliotjkantor@gmail.com>
2021-02-04 09:44:36 -0500

    Use card object

--- cards.py
+++ cards.py
@@ -1,11 +1,25 @@
 import random
 
+# decide whether deck has 2 jokers
 hasJokers = True
 
+# all possible values and suits
 values = "King Queen Jack 10 9 8 7 6 5 4 3 2 1".split()
 suits = "Diamonds Spades Hearts Clubs".split()
 
-allCards = [value + " of " + suit 
+# class for a singular card
+class Card:
+    """Card object to add card functionality"""
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        if not "Joker" in (self.value, self.suit):
+            self.name = self.value + " of " + self.suit
+        else:
+            self.name = self.value + " " + self.suit
+
+# make a list of all cards, in a sorted order
+allCards = [Card(value, suit)
     for suit in suits 
     for value in values]
 
@@ -14,6 +28,7 @@ if hasJokers:
     allCards.append("Red Joker")
 
 def reshuffle():
+    """Shuffle all cards and remove cards from the discard"""
     global availableCards
     availableCards = allCards[:]
     random.shuffle(availableCards)
@@ -22,8 +37,10 @@ def reshuffle():
 reshuffle()
 
 def drawCard():
+    """Return one random card from the deck and move the card to the discard pile"""
     chosenCard = random.choice(availableCards)
     availableCards.remove(chosenCard)
+    discard.append(chosenCard)
     return chosenCard
 
 def dealCards(cardsPerPlayer, numPlayers, numDecks=1):
@@ -40,7 +57,7 @@ def discard(card):
     discard.append(card)
     availableCards.remove(card)
 
-if __name__ == "__main__":
-    hands = dealCards(5, 3)
-    for hand in hands:
-        print(hand)
+# if __name__ == "__main__":
+#     hands = dealCards(5, 3)
+#     for hand in hands:
+#         print(hand)
